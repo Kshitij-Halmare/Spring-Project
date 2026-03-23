6357ae1b8dc871616a06f1583b0fd587f7d63ab4
Kshitij Halmare <kshitij.halmare1@gmail.com>
2026-03-24 00:21:20 +0530

    feat:Kshitij Controller done

---
+++ src/main/java/com/example/Controller_Test_Github/ControllerTestGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.Controller_Test_Github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerTestGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerTestGithubApplication.class, args);
+	}
+
+}
